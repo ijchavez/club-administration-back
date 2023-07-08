87641436079cc64c2b02b19297d8d31925c8fabd
this.gerardochavez <info.gerardo.chavez@gmail.com>
2023-07-08 15:48:44 -0300

    reversing 4th try

--- src/main/java/uy/com/club/administration/config/WebSecurityConfig.java
+++ src/main/java/uy/com/club/administration/config/WebSecurityConfig.java
@@ -64,9 +64,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         final CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(origin);
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE","OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "*"));
         configuration.setAllowCredentials(true);
-        config.setExposedHeaders(Collections.singletonList("Authorization"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "*"));
 
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
